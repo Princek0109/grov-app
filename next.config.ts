8b72f920d2c7fcf2484150ff4da3b39162c13417
Princek0109 <pk0024533@gmail.com>
2026-05-15 23:36:52 +0530

    last

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
-
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-    typescript: {
-      ignoreBuildErrors: true
-    },
-   
-  images: {
-      remotePatterns: [
-          { hostname: 'img.clerk.com'}
-      ]
-  }
+typescript: {
+    ignoreBuildErrors: true,
+},
+
+ images: {
+  remotePatterns: [
+    {hostname: 'img.clerk.com'},
+  ],
+}
 };
 
+export default nextConfig;
