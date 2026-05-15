540773998c6e50221d881e7c74285aacdb5b8826
Princek0109 <pk0024533@gmail.com>
2026-05-16 01:28:14 +0530

    updated logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,10 +9,11 @@ const Navbar = () => {
             <Link href="/">
                 <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image
-                        src="/images/logo.svg"
+                        src="/images/log.png"
                         alt="logo"
-                        width={46}
-                        height={44}
+                        width={86}
+                        height={84}
+        
                     />
                 </div>
             </Link>
